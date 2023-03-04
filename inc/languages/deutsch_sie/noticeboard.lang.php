e5807d33cd37a668c4e9b72c4eb5d7371fb9cfa9
Tom <90357560+whitera-bit@users.noreply.github.com>
2023-03-04 21:51:41 +0100

    Create noticeboard.lang.php

---
+++ inc/languages/deutsch_sie/noticeboard.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$l['noticeboard'] = "Auftragstafeltafel";
+$l['noticeboard_add'] = "Auftrag hinzufügen";
+$l['noticeboard_description'] = "Das ist eine lange Beschreibung der Anschlagstafel.";
+
+?>
